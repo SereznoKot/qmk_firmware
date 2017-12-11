5c6b310b7baf4c52021245f2ff1a4ca92707bf4c
Alexey Pyzhianov <alexey.pyzhianov@mathrioshka.ru>
2017-12-11 19:02:33 +0300

    Minus, plus and asterix

--- keyboards/ergodox_ez/keymaps/neo/keymap.c
+++ keyboards/ergodox_ez/keymaps/neo/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |  \   |  (   |   )  |  +   |  F5  |           |  F6  |  !   |  $   |  #   |  %   |  "   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  ^   |  |   |  {   |   }  |  *   |------|           |------|  -   |  =   |  ~   |  _   |  '   |        |
+ * |        |  ^   |  |   |  {   |   }  |  -   |------|           |------|  *   |  =   |  ~   |  _   |  '   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |(LCtl)|  /   |  [   |   ]  |      |      |           |      |      |  &   |  @   |  ?   |  `   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        KC_NO,   KC_NO,    KC_F1,     KC_F2,   KC_F3,   KC_F4,    KC_NO,
        KC_NO,   KC_NO,    KC_BSLASH, KC_LPRN, KC_RPRN, KC_PLUS,  KC_F5,
-       KC_NO,   KC_CIRC,  KC_PIPE,   KC_LCBR, KC_RCBR, KC_ASTR,
+       KC_NO,   KC_CIRC,  KC_PIPE,   KC_LCBR, KC_RCBR, KC_MINS,
        KC_NO,   KC_TRNS,  KC_SLASH,  KC_LBRC, KC_RBRC, KC_NO,    KC_NO,
        KC_NO,   KC_NO,    KC_NO,     KC_LT,   KC_GT,
                                                       KC_NO, KC_NO,
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_NO,  KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
        KC_F6,  KC_EXLM, KC_DLR,  KC_HASH, KC_PERC,  KC_DQT,  KC_NO,
-               KC_EQL,  KC_MINS, KC_TILD, KC_UNDS,  KC_QUOT, KC_NO,
+               KC_EQL,  KC_ASTR, KC_TILD, KC_UNDS,  KC_QUOT, KC_NO,
        KC_NO,  KC_NO,   KC_AMPR, KC_AT,   KC_QUES,  KC_GRV,  KC_NO,
                         KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
        KC_NO,  KC_NO,
